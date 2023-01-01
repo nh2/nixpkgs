2e055e16cd6d62ed51fa6dc1d3b2af5d724d7f3e
Niklas Hambüchen <mail@nh2.me>
2023-01-02 00:13:11 +0100

    python3Packages.pykep: init at 2.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8675,6 +8675,8 @@ self: super: with self; {
 
   pykerberos = callPackage ../development/python-modules/pykerberos { };
 
+  pykep = callPackage ../development/python-modules/pykep { };
+
   pykeyatome = callPackage ../development/python-modules/pykeyatome { };
 
   pykira = callPackage ../development/python-modules/pykira { };
